eb51a5580db8953c86a67e6bec489bc5a59508b7
QuattraCoding <Benjamin.froberg@gmail.com>
2024-05-11 15:09:48 +0200

    second push and commit

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,2 +1,90 @@
-package org.example;public class Client {
+package org.example;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+
+public class Client {
+
+    ObjectInputStream objectInputStream = null;
+    ObjectOutputStream objectOutputStream = null;
+    Socket socket = null;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    InetAddress host = InetAddress.getLocalHost();
+
+    public Client() throws UnknownHostException, IOException, InterruptedException, ClassNotFoundException {
+        runClient();
+    }
+
+    public void runClient() throws IOException, ClassNotFoundException, InterruptedException {
+
+
+
+        /*
+
+        KOD FÖR E-NIVÅ
+
+        for(int i=0; i<5;i++){
+
+            socket = new Socket(host.getHostName(), 9876);
+
+
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            out.println("Sending request to Socket Server");
+            out.println("Type bye to exit, Write what you want to send to the server: ");
+            String message  = br.readLine();
+            objectOutputStream.writeObject(message);
+
+
+
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            message = (String) objectInputStream.readObject();
+            out.println("Message: " + message);
+
+            objectInputStream.close();
+            objectOutputStream.close();
+            Thread.sleep(100);
+        }
+
+        ------------------------------------------------------------------------------------------------------
+
+            KOD SOM ANVÄNDES FRÅN BÖRJAN. INTE E-NIVÅ KODEN
+
+        bufferedReader = null;
+        bufferedWriter = null;
+        socket = null;
+
+        for (int i = 0; i<5; i++){
+
+
+
+            socket = new Socket(host.getHostAddress(), 1234);
+
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            System.out.println("sending request to server");
+
+            if (i==4){
+                bufferedWriter.write("exit");
+            }
+            else{
+                bufferedWriter.write(" "+i);
+            }
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = bufferedReader.readLine();
+            System.out.println("message: " + message);
+
+            bufferedWriter.close();
+            bufferedReader.close();
+            socket.close();
+            Thread.sleep(100);
+        }
+*/
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        Client client = new Client();
+    }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,2 +1,98 @@
-package org.example;public class Server {
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+    static ServerSocket serverSocket;
+
+    public static int port = 1234;
+
+    static {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+     Socket socket;
+     BufferedReader bufferedReader;
+     BufferedWriter bufferedWriter;
+
+    ArrayList<Client> clients = new ArrayList<>();
+
+    public Server() throws IOException {
+
+    }
+
+    public void runServer() throws IOException, ClassNotFoundException {
+
+
+
+        /*
+                KOD FÖR E-NIVÅ
+
+        //create the socket server object
+        //keep listens indefinitely until receives 'exit' call or program terminates
+        while(true){
+            System.out.println("Waiting for the client request");
+            //creating socket and waiting for client connection
+            Socket socket = serverSocket.accept();
+            //read from socket to ObjectInputStream object
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            //convert ObjectInputStream object to String
+            String message = (String) ois.readObject();
+            System.out.println("Message Received: " + message);
+            //create ObjectOutputStream object
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            //write object to Socket
+            oos.writeObject("Hi Client "+message);
+            //close resources
+            ois.close();
+            oos.close();
+            socket.close();
+            //terminate the server if client sends exit request
+            if(message.equalsIgnoreCase("exit")) break;
+
+            ----------------------------------------------------------------------------------------------
+
+            KOD SOM JAG FÖRSÖKTE MED FRÅN BÖRJAN. iNTE E-NIVÅ
+
+       System.out.println("waiting for client to connect");
+        while(true){
+            socket  = serverSocket.accept();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String message = bufferedReader.readLine();
+            System.out.println("Message Received: " + message);
+
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("hi client" + message);
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            bufferedReader.reset();
+            bufferedReader.close();
+            socket.close();
+
+            if (message.equalsIgnoreCase("exit")) break;
+        }
+        System.out.println("Shutting down server!");
+        serverSocket.close();
+
+        }
+        // */
+
+
+}
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Server server =  new Server();
+        server.runServer();
+
+    }
 }
